7d4de22ba09a22b8f0d202d0b4c040d91f9f0f0c
er10n2 <erionnttelaku@hotmail.com>
2025-12-31 01:48:29 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function showSideBar(){
+
+
+    const sideBar = document.getElementsByClassName("sideBar")[0];
+
+     sideBar.style.display = 'flex';
+
+
+
+
+}
+
+function hideSideBar(){
+    const sideBar = document.getElementsByClassName("sideBar")[0];
+
+     sideBar.style.display = 'none';
+
+}
+
